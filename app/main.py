cd599d20e30d5bf35893fe2d3232b8bed51af0bc
Vanessa Ho <vanessa.ho1@ingka.ikea.com>
2022-02-20 16:22:09 +0100

    Connect app to postgres db with routes using sql queries

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel, validator
 
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
 
 app = FastAPI()
 
@@ -13,6 +17,18 @@ class Post(BaseModel):  # Post class extends BaseModel
     published: bool = True  # if user doesn't provide published value, use 'True' as default
     rating: Optional[int] = None # if user doesn't provide value, defaults to None
 
+while True:
+
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi_db', user='root', password='root', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successful")
+        break
+
+    except Exception as error:
+        print("Connection to db failed")
+        print("Error: ", error)
+        time.sleep(2)
 
 my_posts = [{"title": "Post 1 Title", "content": "Post1 Content", "id": 123},
 {"title": "Post 2 Title", "content": "Post2 Content", "id": 234}]
@@ -36,52 +52,52 @@ async def root():  # function that implements route
     return {"message": "Hello World 123"}
 
 
-@app.get("/posts")
+@app.get("/posts/")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts """)
+    posts = cursor.fetchall()
+    return {"data": posts}
 
 
-@app.post("/create_posts", status_code=status.HTTP_201_CREATED)
-def create_posts(new_post: Post):  # stores post req data in new_post var, validates against Post schema
-    new_post_dict = new_post.dict() # converts response new_post into dict
-    new_post_dict['id'] = randrange(0, 1000000) # assigns post an id
-    my_posts.append(new_post_dict)
-    print(new_post)
-    print(new_post.dict())
+@app.post("/posts/", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):  # stores post req data in new_post var, validates against Post schema
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
+    new_post = cursor.fetchone() 
+    conn.commit()  # saves changes to postgres db
+    
     return {"data": new_post}
 
 
 @app.get("/posts/{id}")
 def get_post(id: int):  # ensure id from params is retrieved as integer
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
+    post = cursor.fetchone()
+    print(post)
     if not post:
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"message": f"post with id: {id} not found"}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id: {id} not found")
     return {"post_detail": post}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    # find index in array that has post id
-    # remove from index with array.pop
-    index = find_index_post(id)
-    if not index:
+    cursor.execute("""DELETE FROM posts where id = %s RETURNING *""", (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} not found")
-    my_posts.pop(index)
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, (post.title, post.content, post.published, str(id),))
+    updated_post = cursor.fetchone()
+    conn.commit()
 
-    if not index:
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} not found")
+    print(updated_post)
     
-    post_dict = post.dict()
-    post_dict["id"] = id
-    my_posts[index] = post_dict
-
-    return {"data": post_dict}
+    return {"data": updated_post}
